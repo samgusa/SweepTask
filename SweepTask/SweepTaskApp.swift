2d3d93202448bc937d345becd3308968b11835b0
samgusa <samgusaapp1993@gmail.com>
2024-07-23 15:07:02 -0400

    Initial Commit

---
+++ SweepTask/SweepTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweepTaskApp.swift
+//  SweepTask
+//
+//  Created by Sam Greenhill on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SweepTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
